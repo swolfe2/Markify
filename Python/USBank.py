717694967364e6d1e85c3b7155d9f7f7714a2e56
swolfe2 <36700028+swolfe2@users.noreply.github.com>
2020-05-20 11:26:37 -0400

    Change to

--- Python/USBank.py
+++ Python/USBank.py
@@ -40,6 +40,7 @@ from turbodbc import connect, make_options
 from io import StringIO
 from datetime import datetime
 import time
+from shutil import copyfile
 
 #Set starting directory
 startDir = '\\\\USTCA097\\Stage\\Database Files\\USBank\\1 - Files To Process'
@@ -50,14 +51,22 @@ for filename in os.listdir(startDir):
         filepath = startDir +'\\'+ filename
         os.remove(filepath)
 
-#Set direct
+#Set directory which has files
+fileDir = '\\\\sappa4fs.kcc.com\\interfaces\\PA4\\Mulesoft\\EDI\\USBANK\\IN\\Processed'
+
+#Loop through all files, and if it's a PAID .txt file that was modified in the last 24 hours, copy to the Files to Process folder
+for filename in os.listdir(fileDir):
+    now = datetime.today()
+    if filename.endswith('.txt') and "PAID" in filename.upper():
+        filepath = fileDir +'\\'+ filename
+        modDate = datetime.fromtimestamp(os.path.getmtime(filepath))
+        hoursOld = (now - modDate).total_seconds() / 3600
+        if hoursOld != 24 and os.path.getsize(filepath) > 0:
+                destfilepath = startDir +'\\'+ filename
+                copyfile(filepath, destfilepath)
 
 #Set completed directory
 compDir = '\\\\USTCA097\\Stage\\Database Files\\USBank\\2 - Completed Files'
-
-
-
-
 
 # Variables to hold record types when parsed
 master = ''
