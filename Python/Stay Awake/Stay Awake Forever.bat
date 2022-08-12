7e1dd5704f65eda9b99a747d81db5cae1659815a
swolfe2 <36700028+swolfe2@users.noreply.github.com>
2022-08-12 09:21:40 -0400

    Stay Awake Forever addition

---
+++ Python/Stay Awake/Stay Awake Forever.bat
@@ -0,0 +1,12 @@
+@echo off
+
+ECHO Running Python Program -Regular
+start /min "" "C:\Program Files\Python37\Python.exe" "C:\Users\%username%\Desktop\Code\Python\Stay Awake\Stay Awake Forever.py"
+if %ERRORLEVEL% neq 0 goto ProcessError-Python
+
+:ProcessError-Python
+Running Python Program -OneDrive
+start /min "" "C:\Program Files\Python37\Python.exe" "C:\Users\%username%\OneDrive - Kimberly-Clark\Desktop\Code\Python\Stay Awake\Stay Awake Forever.py"
+
+ECHO Regular version did not work, had to use OneDrive
+
