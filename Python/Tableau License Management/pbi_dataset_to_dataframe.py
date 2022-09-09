ff3b82b4edf04169a1d52d6ad54795080aa40527
Steve Wolfe <swolfe2@gmail.com>
2022-09-09 14:03:26 -0400

    Delete file

--- Python/Tableau License Management/pbi_dataset_to_dataframe.py
+++
@@ -1,7 +0,0 @@
-import SSAS as ssas_api
-
-xmla_ep = "powerbi://api.powerbi.com/v1.0/myorg/GL%20HR%20-%20Public"
-d_set = "AD User Data Flow"
-
-ssas_api._load_assemblies()  # this uses Windows Authentication
-conn = ssas_api.set_conn_string(server=xmla_ep, db_name=d_set)
