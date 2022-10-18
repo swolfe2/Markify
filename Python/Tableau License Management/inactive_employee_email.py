6f98a938d449596142d941b6d26394e924f2c992
Steve Wolfe <swolfe2@gmail.com>
2022-10-18 16:16:10 -0400

    Simply a test to check Playwright functionality

---
+++ Python/Tableau License Management/inactive_employee_email.py
@@ -0,0 +1,187 @@
+import datetime
+from pathlib import Path
+
+import pandas as pd
+
+import utils.mssql_database as mssql_database  # module in utils folder
+from utils.send_email import send_email  # module in utils folder
+
+
+def main():
+
+    # Connect to MSSQL server
+    conn = mssql_database.connect_to_database()
+
+    # Create a list of columns and data for iterating
+    data_dict = mssql_database.execute_query_to_dictonary(
+        conn,
+        "SELECT * FROM TableauLicenses.dbo.v_RegisteredInactiveKCEmployee WHERE EmailSentOn IS NULL",
+    )
+
+    # Get current python file name, for MSSQL appends
+    current_filename = Path(__file__).name
+
+    # Loop though list, and send email from variable values
+    if len(data_dict) > 0:
+        print("Starting to loop through " + str(len(data_dict)) + " mismatches.")
+
+        today = datetime.date.today()
+        friday = today + (datetime.timedelta((11 - today.weekday()) % 14))
+
+        # Get specific fields from the data dictionary of the index
+        for index in range(len(data_dict)):
+
+            key_name = data_dict[index]["KeyName"]
+            period_end = str(data_dict[index]["PeriodEnd"])
+            assigned_user = data_dict[index]["AssignedName"]
+            assigned_email = data_dict[index]["AssignedEmail"].lower()
+            last_registered_user_name = data_dict[index]["LastRegisteredUserName"]
+            registered_email = data_dict[index]["RegisteredEmail"].lower()
+            last_installed = str(data_dict[index]["LastInstalled"])
+
+            if data_dict[index]["RegisteredEmailManager"] == None:
+                registered_email_manager = ""
+            else:
+                registered_email_manager = data_dict[index][
+                    "RegisteredEmailManager"
+                ].lower()
+
+            if data_dict[index]["AssignedEmailManager"] == None:
+                assigned_email_manager = ""
+            else:
+                assigned_email_manager = data_dict[index][
+                    "AssignedEmailManager"
+                ].lower()
+
+            if assigned_email_manager == registered_email_manager:
+                to = assigned_email_manager
+            else:
+                to_list = [
+                    assigned_email_manager,
+                    registered_email_manager,
+                ]
+                to = str("; ".join(map(str, to_list)))
+
+            if assigned_email == registered_email:
+                cc = assigned_email
+            else:
+                cc_list = [assigned_email, registered_email]
+                cc = str("; ".join(map(str, cc_list)))
+
+            # Calculate what the original Friday date would have been from where the email was previously sent
+            previously_sent = data_dict[index]["EmailSentOn"]
+
+            if previously_sent != None:
+                previous_friday = previously_sent + (
+                    datetime.timedelta((11 - today.weekday()) % 14)
+                )
+
+            html_greeting = "<p>Hello,</p>"
+
+            html_body = (
+                """
+                <!DOCTYPE html> 
+                <html>
+                <head>
+                <style>
+                #tableau_licenses {font-family: Arial, Helvetica, sans-serif;border-collapse: collapse;width: 70%;}
+                #tableau_licenses td, #tableau_licenses th {border: 1px solid #ddd;padding: 5px;}
+                #tableau_licenses tr:nth-child(even){background-color: #D0D7F9;}
+                #tableau_licenses tr:hover {background-color: #0F2C87; color: white;}
+                #tableau_licenses th {padding-top: 12px;padding-bottom: 12px;text-align: left;background-color: #04AA6D;color: white;}
+                </style>
+                </head>
+                <body>"""
+                + html_greeting
+                + """
+                <p>During a current audit on Tableau licenses, it was discovered that the license below is assigned to an employee who is no longer with Kimberly-Clark and last reported to you.</p>
+                <table id="tableau_licenses">
+                <colgroup><col span="1" style="width: 40%;"><col span="1" style="width: 60%;"></colgroup>
+                <tr><td><b>Tableau License</b></td><td>"""
+                + key_name
+                + """</td></tr>
+                <tr><td><b>License Expiry Date</b></td><td>"""
+                + period_end
+                + """</td></tr>
+                <tr><td><b>Assigned To</b></td><td>"""
+                + assigned_user
+                + """</td></tr>
+                <tr><td><b>Assigned To Email</b></td><td>"""
+                + assigned_email
+                + """</td></tr>
+                <tr><td><b>Registered By</b></td><td>"""
+                + last_registered_user_name
+                + """</td></tr>
+                <tr><td><b>Registered Email</b></td><td>"""
+                + registered_email
+                + """</td></tr>
+                <tr><td><b>Last Registered On</b></td><td>"""
+                + last_installed
+                + """</td></tr>
+                </table>
+                <p>In order to resolve the discrepancy, <span style="background-color: #FFFF00"><b>please reply all to this email by EOB """
+                + str(friday)
+                + """</b></span> 
+                if you intend to have another user utilize this license or not. If no response is received by """
+                + str(friday)
+                + """, then the license will be unassigned and will not be renewed once expired.</p> 
+                <p>Also, for any license moves in the future, please ensure that you are following the license process 
+                available on <a href="https://kimberlyclark.sharepoint.com/sites/c141/Pages/RequestTableauLicense.aspx">Tableau Central</a>.</p>
+                <p>If you have any questions, also feel free to reply.</p>
+                <p>Thank you</p>
+                </body>
+                </html>
+                """
+            )
+
+            # Replace characters in html_body with null
+            char_to_replace = {"                ": "", "\n": "", "\n\n": ""}
+
+            # Iterate over all key-value pairs in dictionary
+            for key, value in char_to_replace.items():
+                # Replace key character with value character in string
+                html_body = html_body.replace(key, value)
+
+            # Standard subject for outbound emails and MSSQL reporting
+            subject = "Tableau License: Inactive Employee Assigned"
+
+            # Set BCC for emails, uncomment to/cc for testing
+            to = "steve.wolfe@kcc.com"
+            cc = "steve.wolfe@kcc.com"
+            bcc = "steve.wolfe@kcc.com"
+
+            # Send formatted email to all recipients
+            send_email(
+                subject=subject,
+                to=to,
+                cc=cc,
+                bcc=bcc,
+                html_body=html_body,
+            )
+
+            # Set SQL query for appending
+            query = (
+                """INSERT INTO TableauLicenses.dbo.tblSentEmails(SentOn, EmailType, LicenseNumber, ToAddresses, CCaddresses, BCCAddresses, Subject, Message)
+            SELECT GETDATE(),'"""
+                + str(current_filename)
+                + """','"""
+                + str(key_name)
+                + """', '"""
+                + str(to)
+                + """','"""
+                + str(cc)
+                + """','"""
+                + str(bcc)
+                + """','"""
+                + str(subject)
+                + """', '"""
+                + str(html_body.replace("\n", ""))
+                + """'"""
+            )
+
+            # Push SQL query to TableauLicenses.dbo.tblSentEmails
+            mssql_database.execute_query(conn, query)
+
+
+if __name__ == "__main__":
+    main()
---
+++ Python/Tableau License Management/tableau_quotes.py
@@ -0,0 +1,127 @@
+import datetime
+from pathlib import Path
+
+import pandas as pd
+
+import utils.mssql_database as mssql_database  # module in utils folder
+from utils.send_email import send_email  # module in utils folder
+
+
+def create_dataframe():
+    """
+    This function will get user input for where an Excel file is located, then perform some validation activity to ensure:
+    1) The file has the correct number of columns
+    2) The file has the correct column headers
+    """
+    # Get the filepath to the Excel file
+    user_input = input("What is the filepath to the Excel file?")
+    # user_input = r"C:\Users\U15405\OneDrive - Kimberly-Clark\Desktop\Licenses.xlsx"
+
+    # Create dataframe from Excel values
+    df = pd.read_excel(user_input)
+
+    # Validate that the appropriate column headers are in place
+    print("Validating Excel column headers...")
+    valid_columns = [
+        "KeyName",
+        "ProductName",
+        "AribaTitle",
+        "PurchasingUnit",
+        "CompanyCode",
+        "ERPReferenceID",
+        "FinalApprovalDate",
+        "TableauQuote",
+        "PurchaseRequest",
+        "OrderID",
+        "FullQuoteAmount",
+        "LicensesInBundle",
+    ]
+
+    # Get dataframe size row/column count
+    count_row, count_col = (
+        df.shape[0],
+        df.shape[1],
+    )
+
+    # Compare the actual column count against how many columns there should be
+    len_valid_columns = len(valid_columns)
+    if count_col != len_valid_columns:
+        print(
+            f"The {input} file currently has {count_col} columns, but should have {len_valid_columns} per the documentation. Please correct, and try again."
+        )
+        sys.exit()
+
+    # Compare each positional header against what it should be
+    excel_columns = df.columns
+    for idx, i in enumerate(excel_columns):
+        valid_column = valid_columns[idx]
+        if i != valid_column:
+            print(
+                f"The {input} file currently has {i} in column {idx + 1}, but it should have {valid_column} per the documentation. Please correct, and try again."
+            )
+            sys.exit()
+
+    return df
+
+
+def push_to_mssql(df, conn):
+    "This process calls the processes from the mssql_database module in the same folder"
+
+    temp_table = "##tblTableauQuotesTemp"
+
+    # Get dataframe size
+    count_row, count_col = (
+        df.shape[0],
+        df.shape[1],
+    )  # gives number of row/column count
+
+    # Get Start Time
+    start_time = datetime.datetime.now()
+
+    # Fill all NA values with something
+    for col in df:
+        # get dtype for column
+        d_type = df[col].dtype
+        # check if it is a number
+        if d_type == int or d_type == float:
+            df[col].fillna(0, inplace=True)
+        else:
+            df[col].fillna("", inplace=True)
+
+    # Create dictionary of columns and data types
+    outputdict = mssql_database.sqlcol(df)
+
+    # Clean dataframe values up
+    mssql_database.clean_dataframe(df)
+
+    # Create a temp table, and push values to it from dataframe
+    mssql_database.create_temp_table(df, conn, temp_table, outputdict)
+
+    # Clean the temp table by values
+    mssql_database.clean_temp_table(df, conn, temp_table)
+
+    # Execute Stored Procedure
+    mssql_database.execute_stored_procedure(conn, "dbo.sp_TableauQuotes")
+
+    # Get End Time
+    end_time = datetime.datetime.now()
+    total_seconds = (end_time - start_time).total_seconds()
+    # stop = (time.time() - start).total_seconds()
+    return print(
+        f"Rows: {count_row} Columns: {count_col} Total Seconds: {total_seconds}"
+    )
+
+
+def main():
+    # Create dataframe
+    df = create_dataframe()
+
+    # Connect to MSSQL server
+    conn = mssql_database.connect_to_database()
+
+    # Appends dataframe to MSSQL Server
+    push_to_mssql(df, conn)
+
+
+if __name__ == "__main__":
+    main()
