51fa007ffc396aefeb46b17b969b96748c6d3dc7
Steve Wolfe <swolfe2@gmail.com>
2023-03-31 08:52:09 -0400

    Update for Python 3.11

--- Python/Stay Awake/Stay Awake Forever.bat
+++ Python/Stay Awake/Stay Awake Forever.bat
@@ -1,12 +1,12 @@
 @echo off
 
 ECHO Running Python Program -Regular
-start /min "" "C:\Program Files\Python310\Python.exe" "C:\Users\%username%\Desktop\Code\Python\Stay Awake\Stay Awake Forever.py"
+start /min "" "C:\Program Files\Python311\Python.exe" "C:\Users\%username%\Desktop\Code\Python\Stay Awake\Stay Awake Forever.py"
 if %ERRORLEVEL% neq 0 goto ProcessError-Python
 
 :ProcessError-Python
 ECHO Running Python Program -OneDrive
-start /min "" "C:\Program Files\Python310\Python.exe" "C:\Users\%username%\OneDrive - Kimberly-Clark\Desktop\Code\Python\Stay Awake\Stay Awake Forever.py"
+start /min "" "C:\Program Files\Python311\Python.exe" "C:\Users\%username%\OneDrive - Kimberly-Clark\Desktop\Code\Python\Stay Awake\Stay Awake Forever.py"
 
 ECHO Regular version did not work, had to use OneDrive
 
