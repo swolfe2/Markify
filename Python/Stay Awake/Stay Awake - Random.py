0a38878fb3188cdaba366fd46e6a7583d45edaee
Steve Wolfe <steve.wolfe@kcc.com>
2025-01-10 12:44:23 -0500

    Updates for new System Icon method keep out of task bar

--- Python/Stay Awake/Stay Awake - Random.py
+++ Python/Stay Awake/Stay Awake - Random.py
@@ -4,89 +4,95 @@ from datetime import datetime
 
 import pyautogui
 
-# Set Start Time
-time_start = "07:00:00"
+# Constants
+TIME_START = "07:00:00"
+TIME_END = "18:00:00"
+PIXEL_MOVE = 5
+MIN_SECONDS = 60
+MAX_SECONDS = 240
 
-# Set End Time
-time_end = "18:00:00"
-
-# Set number of pixels to move
-pixel_move = 5
-
-# Start number of moves counter
+# Variables
 num_moves = 0
+stop_flag = False
+current_message = ""
 
 
 # Method to see if current time is between the time start/time end
-def isNowInTimePeriod(start_time, end_time, now_time, num_moves):
-    if not (now_time >= start_time and now_time <= end_time):
-        print(
-            "Sold work today! "
-            + "\n"
-            + "We ran a total of "
-            + str(num_moves)
-            + " times.",
-            end="\r",
-        )
-        exit()
+def is_now_in_time_period(start_time, end_time, now_time, num_moves):
+    global stop_flag
+    time_string = "time" if num_moves == 1 else "times"
+    if not (start_time <= now_time <= end_time):
+        # Clear the line and print the new message
+        print("\033[2J\033[H", end="")
+        print(f"Sold work today! We ran a total of {num_moves} {time_string}.")
+        stop_flag = True
 
 
 # Refresh console every second, counting down the number of seconds left until next
 def waiting(sleep_seconds):
     sleep_second_counter = 0
+
     while sleep_second_counter <= sleep_seconds:
+        if stop_flag:
+            break
+
+        # Calculate remaining seconds
         sleep_seconds_left = sleep_seconds - sleep_second_counter
-        print(
-            "Moved "
-            + str(num_moves)
-            + " times to keep you safe! "
-            + "Next system movement happening in "
-            + str(sleep_seconds_left)
-            + " seconds. "
-            + "Press Ctrl+C to stop script. ",
-            end="\r",
+
+        # Create strings for messaging
+        time_string = "time" if num_moves == 1 else "times"
+        second_string = "second" if sleep_seconds_left == 1 else "seconds"
+        moved_message = (
+            ""
+            if num_moves == 0
+            else f"Moved {num_moves} {time_string} to keep you safe! "
         )
+
+        message = (
+            f"{moved_message}"
+            f"Next system movement happening in {sleep_seconds_left} {second_string}."
+        )
+
+        print(f"\r{message}", end="")
+
         sleep_second_counter += 1
         time.sleep(1)
 
 
-# Loop until CTRL+C is pushed
-try:
-    while True:
-        # Check to see if it's during work hours. If it's not, then exit.
-        isNowInTimePeriod(
-            time_start, time_end, datetime.now().strftime("%H:%M:%S"), num_moves
-        )
+# Loop between time windows
+while not stop_flag:
+    # Check to see if it's during work hours. If it's not, then exit.
+    is_now_in_time_period(
+        TIME_START, TIME_END, datetime.now().strftime("%H:%M:%S"), num_moves
+    )
 
-        # Wait the right number of seconds, displaying a countdown message for every second
-        waiting(random.randint(60, 240))
+    if stop_flag:
+        break
 
-        # Calculate height and width of screen
-        w, h = list(pyautogui.size())[0], list(pyautogui.size())[1]
+    # Wait the right number of seconds, displaying a countdown message for every second
+    waiting(random.randint(MIN_SECONDS, MAX_SECONDS))
 
-        # Get current position
-        x, y = list(pyautogui.position())[0], list(pyautogui.position())[1]
+    # Calculate height and width of screen
+    w, h = list(pyautogui.size())[0], list(pyautogui.size())[1]
 
-        # Set whether we're going left or right on the screen. Positive = right, negative = left
-        if x + pixel_move <= 1 or x + pixel_move >= w:
-            pixel_move = pixel_move * -1
+    # Get current position
+    x, y = list(pyautogui.position())[0], list(pyautogui.position())[1]
 
-        # If you prefere a mouse jitter, uncomment out below. Otherwise, just stick turning the key on/off really quick.
-        # Also, if you would rather just have the mouse ping pong across the screen, keep the top line of the below
-        """
-        pyautogui.moveTo(x + pixel_move, y)
-        pyautogui.moveTo(x - pixel_move, y)
-        pyautogui.moveTo(x, y)
-        """
+    # Set whether we're going left or right on the screen. Positive = right, negative = left
+    if x + PIXEL_MOVE <= 1 or x + PIXEL_MOVE >= w:
+        PIXEL_MOVE = PIXEL_MOVE * -1
 
-        # Turning num lock on/off is the easiest one to do. However, you can really press any button you want.
-        pyautogui.keyDown("numlock")
-        pyautogui.keyUp("numlock")
-        pyautogui.keyDown("numlock")
-        pyautogui.keyUp("numlock")
-        num_moves += 1
+    # If you prefere a mouse jitter, uncomment out below. Otherwise, just stick turning the key on/off really quick.
+    # Also, if you would rather just have the mouse ping pong across the screen, keep the top line of the below
+    """
+    pyautogui.moveTo(x + PIXEL_MOVE, y)
+    pyautogui.moveTo(x - PIXEL_MOVE, y)
+    pyautogui.moveTo(x, y)
+    """
 
-
-# Pressing CTRL+C will stop script
-except KeyboardInterrupt:
-    pass
+    # Turning num lock on/off is the easiest one to do. However, you can really press any button you want.
+    pyautogui.keyDown("numlock")
+    pyautogui.keyUp("numlock")
+    pyautogui.keyDown("numlock")
+    pyautogui.keyUp("numlock")
+    num_moves += 1
--- Python/Stay Awake/Stay Awake - System Icon.py
+++ Python/Stay Awake/Stay Awake - System Icon.py
@@ -1,117 +1,139 @@
+import os
+import random
+import sys
 import threading
 import time
 from datetime import datetime
-from os import path as path
 
-import PIL.Image
 import pyautogui
 import pystray
+from PIL import Image
+
+# Constants
+TIME_START = "07:00:00"
+TIME_END = "18:00:00"
+PIXEL_MOVE = 5
+MIN_SECONDS = 60
+MAX_SECONDS = 240
+
+# Variables
+num_moves = 0
+stop_flag = False
+current_message = ""
 
 
-def run_icon():
-    # Set main file path for current file
-    dir_path = path.dirname(path.realpath(__file__))
-
-    icon_image = "\\Icon\\Python.ico"
-
-    full_path = dir_path + icon_image
-
-    image = PIL.Image.open(full_path)
-
-    # Handle click events to icon
-    def on_clicked(icon, item):
-        if str(item) == "Quit":
-            icon.stop()
-
-    # Create system tray icon
-    icon = pystray.Icon(
-        "Stay Awake",
-        image,
-        menu=pystray.Menu(
-            pystray.MenuItem("Quit", on_clicked),
-        ),
-    )
-
-    # Start running in system tray
-    icon.run(setup=main_loop())
+# Method to see if current time is between the time start/time end
+def is_now_in_time_period(start_time, end_time, now_time, num_moves):
+    global stop_flag
+    time_string = "time" if num_moves == 1 else "times"
+    if not (start_time <= now_time <= end_time):
+        # Clear the line and print the new message
+        print("\033[2J\033[H", end="")
+        print(f"Sold work today! We ran a total of {num_moves} {time_string}.")
+        stop_flag = True
 
 
-def main_loop():
-
-    # Set Start Time
-    time_start = "07:00:00"
-
-    # Set End Time
-    time_end = "18:00:00"
-
-    # Set sleep second integer
-    sleep_seconds = 240
-
-    # Set number of pixels to move
-    pixel_move = 5
-
-    # Start number of moves counter
-    num_moves = 0
-
-    # Refresh console every second, counting down the number of seconds left until next
-    def wait_loop(sleep_seconds):
-        sleep_second_counter = 0
-        while sleep_second_counter <= sleep_seconds:
-            sleep_seconds_left = sleep_seconds - sleep_second_counter
-            print(
-                "Moved "
-                + str(num_moves)
-                + " times to keep you safe! "
-                + "Next system movement happening in "
-                + str(sleep_seconds_left)
-                + " seconds. "
-                + "Press Ctrl+C to stop script. ",
-                end="\r",
-            )
-            sleep_second_counter += 1
-            time.sleep(1)
-
-    # Method to see if current time is between the time start/time end
-    def check_timeperiod(start_time, end_time, now_time, num_moves):
-        if not (now_time >= start_time and now_time <= end_time):
-            print(
-                "Sold work today! "
-                + "\n"
-                + "We ran a total of "
-                + str(num_moves)
-                + " times, which covered you for "
-                + str(round(int(num_moves) * int(sleep_seconds) / 60, 2))
-                + " minutes.",
-                end="\r",
-            )
-            exit()
-
-    # Loop until CTRL+C is pushed
-    try:
-        while True:
-            # Check to see if it's during work hours. If it's not, then exit.
-            check_timeperiod(
-                time_start, time_end, datetime.now().strftime("%H:%M:%S"), num_moves
-            )
-
-            # Wait the right number of seconds, displaying a countdown message for every second
-            wait_loop(sleep_seconds)
-
-            # Turning num lock on/off is the easiest one to do. However, you can really press any button you want.
-            pyautogui.keyDown("numlock")
-            pyautogui.keyUp("numlock")
-            pyautogui.keyDown("numlock")
-            pyautogui.keyUp("numlock")
-            num_moves += 1
-
-    # Pressing CTRL+C will stop script
-    except KeyboardInterrupt:
-        pass
+# Function to update the tooltip text
+def update_tooltip(icon, message):
+    global current_message
+    current_message = message
+    icon.title = message
 
 
-def main():
-    run_icon()
+# Refresh console every second, counting down the number of seconds left until next
+def waiting(sleep_seconds, icon):
+    sleep_second_counter = 0
+    time_string = "time"
+
+    while sleep_second_counter <= sleep_seconds:
+        if stop_flag:
+            break
+
+        # Calculate remaining seconds
+        sleep_seconds_left = sleep_seconds - sleep_second_counter
+
+        # Create strings for messaging
+        time_string = "time" if num_moves == 1 else "times"
+        second_string = "second" if sleep_seconds_left == 1 else "seconds"
+        moved_message = (
+            ""
+            if num_moves == 0
+            else f"Moved {num_moves} {time_string} to keep you safe! \n"
+        )
+
+        message = (
+            f"{moved_message}"
+            f"Next system movement happening in {sleep_seconds_left} {second_string}."
+        )
+
+        update_tooltip(icon, message)
+        sleep_second_counter += 1
+        time.sleep(1)
 
 
-if __name__ == "__main__":
-    main()
+# Function to run the main script
+def run_script(icon):
+    global num_moves, PIXEL_MOVE
+    while not stop_flag:
+        # Check to see if it's during work hours. If it's not, then exit.
+        is_now_in_time_period(
+            TIME_START, TIME_END, datetime.now().strftime("%H:%M:%S"), num_moves
+        )
+
+        if stop_flag:
+            break
+
+        # Wait the right number of seconds, displaying a countdown message for every second
+        waiting(random.randint(MIN_SECONDS, MAX_SECONDS), icon)
+
+        if stop_flag:
+            break
+
+        # Calculate height and width of screen
+        w, h = pyautogui.size()
+
+        # Get current position
+        x, y = pyautogui.position()
+
+        # Set whether we're going left or right on the screen. Positive = right, negative = left
+        if x + PIXEL_MOVE <= 1 or x + PIXEL_MOVE >= w:
+            PIXEL_MOVE = -PIXEL_MOVE
+
+        # Turning num lock on/off is the easiest one to do, but any button can be pressed.
+        pyautogui.press("numlock")
+        pyautogui.press("numlock")
+        num_moves += 1
+
+    # Stop the icon
+    icon.stop()
+
+
+# Function to quit the application
+def quit_app(icon, item):
+    global stop_flag
+    stop_flag = True
+    icon.stop()
+
+
+# Get the directory of the currently running script
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Construct the path to the icon file
+icon_path = os.path.join(script_dir, "icon", "python.ico")
+
+# Create the system tray icon
+icon = pystray.Icon("test_icon")
+icon.icon = Image.open(icon_path)
+icon.title = os.path.basename(__file__)  # Set the initial tooltip to the script name
+icon.menu = pystray.Menu(pystray.MenuItem("Quit", quit_app))
+
+# Run the script in a separate thread
+thread = threading.Thread(target=run_script, args=(icon,))
+thread.start()
+
+# Run the icon
+icon.run()
+
+# Ensure the icon stops when the script ends
+if stop_flag:
+    icon.stop()
