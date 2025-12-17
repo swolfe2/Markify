a0227da5df153a7fe3bffb93b15c69e5fc1a7263
Steve Wolfe <steve.wolfe@kcc.com>
2025-12-17 07:51:39 -0500

    Updated logic to remove charindex error

--- SQL/PBI Statistics - Azure/PBI Statistics - Workspace Detail.sql
+++ SQL/PBI Statistics - Azure/PBI Statistics - Workspace Detail.sql
@@ -1,83 +1,91 @@
-/*
-This SQL replicates the Segment - ABU Dataflow logic from 
-https://app.powerbi.com/groups/a04966ab-98e7-419b-9374-7ebf0e10fcc4/dataflows/5d9e1dec-5981-4d61-b5f3-389e3dcd8c3e?experience=power-bi
-*/
-/*
-Get the parent domain values
-*/
-WITH ParentDomain
-AS (
-	SELECT dd.DomainID,
-		dd.DisplayName,
-		dd.Description,
-		dd.ContributorsScope,
-		RTRIM(SUBSTRING(dd.DisplayName, 1, CHARINDEX(' - ', dd.DisplayName) - 1)) AS Segment,
-		LTRIM(SUBSTRING(dd.DisplayName, CHARINDEX(' - ', dd.DisplayName) + 3, LEN(dd.DisplayName))) AS ABU
-	FROM PBI_Platform_Automation.DomainDetail dd
-	WHERE ParentDomainID IS NULL
-	GROUP BY dd.DomainID,
-		dd.DisplayName,
-		dd.Description,
-		dd.ContributorsScope
-	),
-/*
-Get the subdomain values
-*/
-SubDomain
-AS (
-	SELECT dd.DisplayName AS "GlobalFunction",
-		dd.DomainID AS "SubdomainID",
-		dd.ContributorsScope AS "SubDomainContributorsScope",
-		dd.[Description] AS "SubDomainDescription",
-		dd.ParentDomainID
-	FROM PBI_Platform_Automation.DomainDetail dd
-	WHERE ParentDomainID IS NOT NULL
-	GROUP BY dd.DomainID,
-		dd.DisplayName,
-		dd.Description,
-		dd.ContributorsScope,
-		dd.ParentDomainID
-	)
+
+-- T-SQL
 
 /*
-Combine Domains, Subdomains and Workspace Details
+Replicates Segment / ABU logic from the referenced Dataflow
+and protects against invalid DisplayName values that lack ' - '.
 */
+
+WITH ParentDomain AS
+(
+    SELECT
+        dd.DomainID,
+        dd.DisplayName,
+        dd.Description,
+        dd.ContributorsScope,
+        -- compute the separator position once
+        CHARINDEX(' - ', dd.DisplayName) AS sep_pos
+    FROM PBI_Platform_Automation.DomainDetail AS dd
+    WHERE dd.ParentDomainID IS NULL
+      AND dd.DisplayName IS NOT NULL
+      AND CHARINDEX(' - ', dd.DisplayName) > 0
+),
+ParsedParent AS
+(
+    SELECT
+        pd.DomainID,
+        pd.DisplayName,
+        pd.Description,
+        pd.ContributorsScope,
+        -- safe Segment and ABU parsing
+        RTRIM(SUBSTRING(pd.DisplayName, 1, pd.sep_pos - 1)) AS Segment,
+        LTRIM(SUBSTRING(pd.DisplayName, pd.sep_pos + 3, LEN(pd.DisplayName) - pd.sep_pos - 2)) AS ABU
+    FROM ParentDomain AS pd
+),
+SubDomain AS
+(
+    SELECT
+        dd.DisplayName           AS GlobalFunction,
+        dd.DomainID              AS SubdomainID,
+        dd.ContributorsScope     AS SubDomainContributorsScope,
+        dd.[Description]         AS SubDomainDescription,
+        dd.ParentDomainID
+    FROM PBI_Platform_Automation.DomainDetail AS dd
+    WHERE dd.ParentDomainID IS NOT NULL
+)
 SELECT 
-	wd.WorkspaceName,
-	wd.WorkspaceID,
-	CASE WHEN LEFT(wd.WorkspaceName, 3) = 'GL ' THEN 'Global'
-		WHEN LEFT(wd.WorkspaceName, 3) = 'AP ' THEN 'Asia Pac'
-		WHEN LEFT(wd.WorkspaceName, 3) = 'NA ' THEN 'North America'
-		WHEN LEFT(wd.WorkspaceName, 4) = 'LAO ' THEN 'Latin America'
-		WHEN LEFT(wd.WorkspaceName, 5) = 'EMEA ' THEN 'EMEA'
-	ELSE 'Naming Error'
-	END AS WorkspaceRegion,
-	CASE WHEN RIGHT(wd.WorkspaceName, 3) = '- D' THEN 'Development'
-		WHEN RIGHT(wd.WorkspaceName, 3) = '- Q' THEN 'Quality'
-		WHEN RIGHT(wd.WorkspaceName, 7) = '- ADHOC' THEN 'Adhoc'
-		WHEN RIGHT(wd.WorkspaceName, 8) = '- PUBLIC' THEN 'Public'
-		WHEN LEFT(wd.WorkspaceName, 3) IN ('GL ', 'AP ', 'NA ', 'LAO', 'EME') THEN 'Production Certified'
-	ELSE 'Naming Error'
-	END AS WorkspaceType,
-	pd.DomainID,
-	sd.SubDomainID,
-	pd.Segment,
-	pd.ABU,
-	sd.GlobalFunction
-FROM ParentDomain pd
-LEFT JOIN SubDomain sd ON pd.DomainID = sd.ParentDomainID
-INNER JOIN PBI_Platform_Automation.DomainWorkspace dw 
-	ON dw.DomainID = sd.SubdomainID
-INNER JOIN PBI_Platform_Automation.WorkspaceDetail wd
-	ON wd.WorkspaceID = dw.WorkspaceID
-	AND wd.IsOnDedicatedCapacity = 1
-GROUP BY dw.WorkspaceID,
-	wd.WorkspaceName,
-	wd.WorkspaceID,
-	pd.Segment,
-	pd.ABU,
-	pd.DomainID,
-	sd.SubDomainID,
-	sd.GlobalFunction,
-	sd.SubDomainDescription,
-	sd.SubDomainContributorsScope
+    wd.WorkspaceName,
+    wd.WorkspaceID,
+    CASE 
+        WHEN LEFT(wd.WorkspaceName, 3) = 'GL '  THEN 'Global'
+        WHEN LEFT(wd.WorkspaceName, 3) = 'AP '  THEN 'Asia Pac'
+        WHEN LEFT(wd.WorkspaceName, 3) = 'NA '  THEN 'North America'
+        WHEN LEFT(wd.WorkspaceName, 4) = 'LAO ' THEN 'Latin America'
+        WHEN LEFT(wd.WorkspaceName, 5) = 'EMEA ' THEN 'EMEA'
+        ELSE 'Naming Error'
+    END AS WorkspaceRegion,
+    CASE 
+        WHEN RIGHT(wd.WorkspaceName, 3) = '- D'      THEN 'Development'
+        WHEN RIGHT(wd.WorkspaceName, 3) = '- Q'      THEN 'Quality'
+        WHEN RIGHT(wd.WorkspaceName, 7) = '- ADHOC'  THEN 'Adhoc'
+        WHEN RIGHT(wd.WorkspaceName, 8) = '- PUBLIC' THEN 'Public'
+        -- treat Region prefixes as Production Certified
+        WHEN LEFT(wd.WorkspaceName, 3) IN ('GL ', 'AP ', 'NA ') 
+          OR LEFT(wd.WorkspaceName, 4) = 'LAO '
+          OR LEFT(wd.WorkspaceName, 5) = 'EMEA '
+          THEN 'Production Certified'
+        ELSE 'Naming Error'
+    END AS WorkspaceType,
+    pp.DomainID,
+    sd.SubdomainID,
+    pp.Segment,
+    pp.ABU,
+    sd.GlobalFunction
+FROM ParsedParent AS pp
+LEFT JOIN SubDomain   AS sd ON pp.DomainID = sd.ParentDomainID
+INNER JOIN PBI_Platform_Automation.DomainWorkspace AS dw 
+    ON dw.DomainID = sd.SubdomainID
+INNER JOIN PBI_Platform_Automation.WorkspaceDetail AS wd
+    ON wd.WorkspaceID = dw.WorkspaceID
+   AND wd.IsOnDedicatedCapacity = 1
+GROUP BY 
+    dw.WorkspaceID,
+    wd.WorkspaceName,
+    wd.WorkspaceID,
+    pp.Segment,
+    pp.ABU,
+    pp.DomainID,
+    sd.SubdomainID,
+    sd.GlobalFunction,
+    sd.SubDomainDescription,
+    sd.SubDomainContributorsScope
